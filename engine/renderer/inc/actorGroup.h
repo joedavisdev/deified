df9fe369b929f88ce8e6a3fc7d7fe62e0ca8d321
Joe Davis <joemdavis@gmail.com>
2015-08-29 19:12:39 +0100

    Refactor: Mesh* and Pipeline* are now __weak pointers

--- engine/renderer/inc/actorGroup.h
+++ engine/renderer/inc/actorGroup.h
@@ -5,14 +5,14 @@
 
 @interface ActorGroup : NSObject {
 }
-@property (nonatomic, readonly) Mesh* mesh;
+@property (nonatomic, readonly) __weak Mesh* mesh;
 @property (nonatomic, readonly) void* bodyPtrs;
 - (id)initWithMeshAndBodies: (Mesh*)mesh bodies: (const void*)bodies numberOfBodies:(NSUInteger)numberOfBodies;
 @end
 
 @interface ConstantBufferGroup : NSObject {
 }
-@property (nonatomic, readonly) Pipeline* pipeline;
+@property (nonatomic, readonly) __weak Pipeline* pipeline;
 @property (nonatomic, readonly) NSMutableArray* actorGroupPtrs;
 - (id)initPipelineAndActorGroups: (id<MTLDevice>)device pipeline:(Pipeline*)pipeline uniformBlockSize:(NSUInteger)uniformBlockSize actorGroups: (NSMutableArray*)actorGroups;
 - (id<MTLBuffer>)getConstantBuffer: (NSUInteger)bufferIndex;
