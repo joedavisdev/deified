529a8555ef4b8bbe56c0b6defd8765823a150e01
Joe Davis <joemdavis@gmail.com>
2015-09-20 16:19:17 +0100

    Refactor: Renamed CubeLighting to BasicLighting

--- demos/02_BasicPODScene/src/UniformBlocks.h
+++ demos/02_BasicPODScene/src/UniformBlocks.h
@@ -4,7 +4,7 @@
 #ifdef __cplusplus
 namespace JMD {
 namespace UB { // Uniform block
-    struct CubeLighting {
+    struct BasicLighting {
         simd::float4x4 modelview_projection_matrix;
         simd::float4x4 normal_matrix;
         simd::float4   ambient_color;
