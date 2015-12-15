c11ea0bfa9bcca474022c755c2de3546140f3292
Joe Davis <joemdavis@users.noreply.github.com>
2015-12-15 23:07:06 +0000

    Feature: Now loads Actor instances Fix: Added a Model ReleaseData function Refactor: Moved function definitions to the .cpp Refactor: models_ is now an std::unordered_map

--- engine/core/inc/scene_man.hpp
+++ engine/core/inc/scene_man.hpp
@@ -1,67 +1,49 @@
 #pragma once
 
 #include <vector>
+#include <unordered_map>
 #include <string>
 
+#include "glm/glm.hpp"
+
 namespace JMD {
-    struct RenderPass {};
-    struct SMPipeline {};   // TEMP: SM prefix to avoid name clashes
-    struct SMMesh {         // TEMP: SM prefix to avoid name clashes
-        unsigned int    num_vertices_;
-        unsigned int    stride_;
-        unsigned int    num_indices_;
-        unsigned int    num_indices_bytes_;
-        char*           vertices_;
-        char*           indices_;
-        
-        SMMesh()
-        :
-        num_vertices_(0),
-        stride_(0),
-        num_indices_(0),
-        num_indices_bytes_(0),
-        vertices_(nullptr),
-        indices_(nullptr)
-        {}
-        SMMesh(char* vertices,
-               unsigned int num_vertices,
-               unsigned int stride,
-               char* indices,
-               unsigned int num_indices,
-               unsigned int num_indices_bytes)
-        :
-        num_vertices_(num_vertices),
-        stride_(stride),
-        num_indices_(num_indices),
-        num_indices_bytes_(num_indices_bytes)
-        {
-            vertices_ = new char[num_vertices];
-            memcpy(vertices_,vertices,num_vertices);
-            indices_ = new char[num_indices];
-            memcpy(indices_,indices,num_indices);
-        }
-        ~SMMesh() {
-        }
-        void ReleaseData() {
-            delete[] vertices_;vertices_=nullptr;
-            delete[] indices_;indices_=nullptr;
-        }
-    };
-    struct Model {
-        unsigned int number_of_meshes;
-        SMMesh* mesh_array;
-        Model()
-        :
-        number_of_meshes(0),
-        mesh_array(nullptr)
-        {}
-        ~Model(){
-            delete []mesh_array;
-        }
-    };
-    struct Actor {
-        std::string name;
-    };
+struct RenderPass {};
+struct SMPipeline {};   // TEMP: SM prefix to avoid name clashes
+class SMMesh {         // TEMP: SM prefix to avoid name clashes
+public:
+    unsigned int    num_vertices_;
+    unsigned int    stride_;
+    unsigned int    num_indices_;
+    unsigned int    num_indices_bytes_;
+    char*           vertices_;
+    char*           indices_;
+    
+    SMMesh();
+    SMMesh(char* vertices,
+           unsigned int num_vertices,
+           unsigned int stride,
+           char* indices,
+           unsigned int num_indices,
+           unsigned int num_indices_bytes);
+    ~SMMesh();
+    void ReleaseData();
+};
+class Model {
+public:
+    unsigned int number_of_meshes;
+    SMMesh* mesh_array;
+    Model();
+    ~Model();
+    void ReleaseData();
+};
+struct PhysicsBody {
+    glm::vec4 position;
+};
+struct Actor {
+    std::string name;
+    PhysicsBody body;   // World space position etc.
+    Model* model;       // A renderable collection of meshes
+};
 class SceneMan {
 public:
     ~SceneMan();
@@ -72,7 +54,7 @@ public:
 private:
     std::vector<RenderPass> render_passes_;
     std::vector<SMPipeline> pipelines_;
-    std::vector<Model> models_;
+    std::unordered_map<std::string,Model> models_;
     std::vector<Actor> actors_;
     
     void ReleaseData();
