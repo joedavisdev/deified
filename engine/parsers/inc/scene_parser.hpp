9602373c9717d8cb63d23797f2b2f874c4dac029
Joe Davis <joemdavis@users.noreply.github.com>
2015-12-27 16:48:06 +0000

    Feature: Render pass parser

--- engine/parsers/inc/scene_parser.hpp
+++ engine/parsers/inc/scene_parser.hpp
@@ -17,13 +17,21 @@ namespace JMD {
         std::string model_name;
         glm::vec4   world_position;
     };
+    struct ParsedRenderPass {
+        std::string name;
+        std::string actor_regex;
+        std::vector<std::string> colour_attachments;
+        std::string depth_stencil_attachments;
+    };
 class SceneParser {
 public:
     void Parse(const std::string &scene_file);
     std::vector<ParsedEffect> effects;
     std::vector<ParsedActor> actors;
+    std::vector<ParsedRenderPass> render_passes;
 private:
     void ProcessEffects(const Json::Value &effects_node);
     void ProcessActors(const Json::Value &actors_node);
+    void ProcessRenderPasses(const Json::Value &render_pass_node);
 };
 } // namespace JMD
--- engine/parsers/src/scene_parser.cpp
+++ engine/parsers/src/scene_parser.cpp
@@ -1,5 +1,7 @@
 #include "scene_parser.hpp"
 
+#include <utility>
+
 namespace JMD {
     // Code nabbed from here: https://ysonggit.github.io/coding/2014/12/16/split-a-string-using-c.html
     std::vector<std::string> StringSplit(const std::string &s, char delim) {
@@ -20,7 +22,7 @@ namespace JMD {
             parsed_effect.name = value_object["name"].asString();
             parsed_effect.vert_shader_name = value_object["vert_shader"].asString();
             parsed_effect.frag_shader_name = value_object["frag_shader"].asString();
-            effects.push_back(parsed_effect);
+            effects.push_back(std::move(parsed_effect));
         }
     }
     void SceneParser::ProcessActors(const Json::Value &actors_node) {
@@ -37,7 +39,26 @@ namespace JMD {
                                                     atof(world_position_string[1].c_str()),
                                                     atof(world_position_string[2].c_str()),
                                                     1.0);
-            actors.push_back(parsed_actor);
+            actors.push_back(std::move(parsed_actor));
+        }
+    }
+    void SceneParser::ProcessRenderPasses(const Json::Value &render_pass_node) {
+        if(!render_pass_node.isArray()) return;
+        for(unsigned int index=0; index < render_pass_node.size(); ++index) {
+            const Json::Value value_object(render_pass_node[index]);
+            if(!value_object.isObject()) continue;
+            ParsedRenderPass parsed_render_pass;
+            parsed_render_pass.name = value_object["name"].asString();
+            parsed_render_pass.actor_regex = value_object["actor_regex"].asString();
+            parsed_render_pass.depth_stencil_attachments = value_object["depth_stencil_attachments"].asString();
+            auto colour_attachments(value_object["colour_attachments"]);
+            if(colour_attachments.isArray()){
+                for(unsigned int attachment_index=0; attachment_index < colour_attachments.size(); ++attachment_index) {
+                    const Json::Value attachment_object(colour_attachments[attachment_index]);
+                    parsed_render_pass.colour_attachments.push_back(std::move(attachment_object.asString()));
+                }
+            }
+            render_passes.push_back(std::move(parsed_render_pass));
         }
     }
     void SceneParser::Parse(const std::string &scene_file) {
@@ -45,5 +66,6 @@ namespace JMD {
         Json::Reader json_reader;json_reader.parse(scene_file, root);
         ProcessEffects(root.get("effects", default_node));
         ProcessActors(root.get("actors", default_node));
+        ProcessRenderPasses(root.get("render_passes", default_node));
     }
 }
